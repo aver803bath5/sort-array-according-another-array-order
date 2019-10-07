94129b9d3ff24b96f01b6794748260b041d5edf8
g9Mien <aver803bath261@gmail.com>
2019-10-07 18:24:50 +0800

    Add hash-map method

---
+++ hash-map/index.js
@@ -0,0 +1,42 @@
+// Generate a hash map which structure is (key: arr1's element, value: occurence of the key number)
+function genHashMap(arr1, arr2) {
+  const hashMap = new Map(arr2.map((element) => [element, 0]));
+
+  arr1.forEach((element) => {
+    if (hashMap.get(element) !== undefined) {
+      hashMap.set(element, hashMap.get(element) + 1);
+    } else {
+      hashMap.set(element, 0);
+    }
+  });
+
+  return hashMap;
+}
+
+function sortArray(arr1, arr2) {
+  let result = [];
+  const hashMap = genHashMap(arr1, arr2);
+  arr2.forEach((element) => {
+    if (hashMap.get(element)) {
+      // Get occurence of the matched element from hash map and assign to count
+      // then push the element into result[] `count` times
+      let count = hashMap.get(element);
+      while (count !== 0) {
+        result.push(element);
+        count -= 1;
+      }
+      // Remove the element which has been pushed into result[] from the hash map
+      hashMap.delete(element);
+    }
+  });
+
+  const unmathedElements = Array.from(hashMap.keys()).sort((a, b) => a - b);
+  result = result.concat(unmathedElements);
+
+  return result;
+}
+
+const arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19];
+const arr2 = [2, 1, 4, 3, 9, 6];
+
+console.log(sortArray(arr1, arr2));
